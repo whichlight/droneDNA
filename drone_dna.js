08e0b6ef3075c5968fe2cd78305f550952f495c3
Kawandeep Virdee <Kawandeep@embed.ly>
2013-05-02 20:59:14 -0400

    reading in data

--- drone_dna.js
+++ drone_dna.js
@@ -7,14 +7,36 @@ $(document).ready(function(){
 
   }
 
-
   'use strict';
 
 
-  s1= get_synth(35);
-  s2=get_synth(35.5);
- s3=get_synth(36);
+  s1 = get_synth(35);
+  s2 = get_synth(70);
 
+ // s1.volume.gain.value=0.2;
+    $.getJSON("data/paramlist100.txt",function(data){
+        $.each(data, function(key, val){
+           setTimeout(function(){process_data(val)},key*100);
+        });
+    });
+
+
+    function process_data(val){
+        var osc1 = remap(val.osc1, 0,100, 30,40);
+        var osc2 = remap(val.osc2, 0,100, 30,40);
+        var amp1 = remap(val.amp1, 0,100, 0,1);
+        var amp2 = remap(val.amp1, 0,100, 0,1);
+
+        s1.source.frequency.value = osc1;
+        s2.source.frequency.value = osc2;
+        s1.volume.gain.value = amp1;
+        s2.volume.gain.value = amp2;
+        console.log(val);
+    }
+
+    function remap(val, i1, i2, f1, f2){
+        return f1 + (val-i1)*(f2-f1)/(i2-i1);
+    }
 
     function get_synth(pitch){
         nodes={};
@@ -27,15 +49,14 @@ $(document).ready(function(){
         nodes.source.connect(nodes.filter);
         nodes.filter.connect(nodes.volume);
 
-
         nodes.volume.connect(myAudioContext.destination);
-
 
         //pitch val
         nodes.source.frequency.value=pitch;
 
         //frequency val
         nodes.filter.frequency.value=100;
+
         nodes.source.noteOn(0);
         return nodes
     }
